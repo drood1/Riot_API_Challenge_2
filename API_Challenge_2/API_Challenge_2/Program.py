6c072a8db5e0f92de06dc0ac5334ef4cf345019a
Jason Lee <calccrypto@gmail.com>
2015-08-14 20:25:41 -0400

    Read from JSON file instead of user input

--- API_Challenge_2/API_Challenge_2/Program.py
+++ API_Challenge_2/API_Challenge_2/Program.py
@@ -2,6 +2,7 @@ import json
 import os
 import urllib2
 
+filename = os.getcwd() + "../../AP_ITEM_DATASET/5.11/NORMAL_5X5/NA.json"
 base_match_url = "https://na.api.pvp.net/api/lol/na/v2.2/match/"
 api_key = "?api_key=72ed6f93-1e5d-47b3-ae92-8c4657887887"
 
@@ -31,14 +32,10 @@ class Item:
     def getAverage(self):
         return self.__average_per_game
 
-    def __str__(self):
-        return "{0}: {1} {2}".format(self.__id, self.__item_name, self.__count)
-
 def printAllItemCount(items):
     for id in sorted(items):
         print("{0} has a count of {1}".format(items[id].getName(), items[id].getCount()))
 
-    # blank line to separate matches
     print("\n")
 
 def main():
@@ -58,35 +55,38 @@ def main():
                 3152: Item("Will of the Ancients",      3152, True),
             }
 
-    game_count = 0
-    while (True):
-        match_id = raw_input("Please give a match id or type 'exit' to exit.")
-        if (match_id == "exit"):
-            break
+    # while (True):
+        # match_id = raw_input("Please give a match id or type 'exit' to exit.")
+        # if (match_id == "exit"):
+            # break
+        # print("match_id number is {0}".format(match_id))
 
-        print("match_id number is {0}".format(match_id))
+    with open(filename, "r") as jsonfile:
+        for match_id in json.load(jsonfile):
+            # FORM URL
+            full_url = base_match_url + str(match_id) + api_key
+            print("URL: {0}".format(full_url))
 
-        # FORM URL
-        full_url = base_match_url + match_id + api_key
-        print("URL: {0}\n".format(full_url))
+            # CALL API WITH FORMED URL
+            response = ""
+            try:
+                response = urllib2.urlopen(full_url)
+            except urllib2.URLError as e:
+                print "{0} is an invalid match id".format(match_id)
+                continue
+                # return
 
-        # CALL API WITH FORMED URL
-        response = ""
-        try:
-            response = urllib2.urlopen(full_url)
-        except urllib2.URLError as e:
-            print "{0} is an invalid match id, please enter a valid match id\n".format(match_id)
-            return
+            data = json.load(response)
 
-        data = json.load(response)
+            # get player stats
+            for player in data["participants"]:
+                stats = player["stats"]
+                for item in range(7):
+                    id = int(stats["item{0}".format(item)])
+                    if id in items:
+                        items[id].Add()
 
-        for player in data["participants"]:
-            stats = player["stats"]
-            for item in range(7):
-                id = int(stats["item{0}".format(item)])
-                if id in items:
-                    items[id].Add()
-
+        # print aggregated data
         printAllItemCount(items)
 
     print("Goodbye.")
